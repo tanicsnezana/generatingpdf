2eb6b031aeda84689ee90981ee5a6c01ef5b3115
Igor Spasić <igor@jodd.org>
2018-04-12 11:11:05 +0200

    Missing jars

--- src/PdfGeneratorUtil.java
+++ src/PdfGeneratorUtil.java
@@ -1,18 +1,17 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.UUID;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.util.Assert;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
-import org.xhtmlrenderer.pdf.ITextRenderer;
-import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
-import com.lowagie.text.DocumentException;
 
 
 @Component
@@ -53,7 +52,7 @@ public class PdfGeneratorUtil {
             renderer.createPDF(os, false);
             renderer.finishPDF();
 
-            System.out.println("PDF created successfully");
+            System.out.println("PDF created successfully: " + outputFile);
         }
         finally {
             if (os != null) {
